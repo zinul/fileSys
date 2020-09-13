26274e4cb69eac23e1894481f22d80a0e5f284c9
zmz <245875755@qq.com>
2020-09-13 17:55:55 +0800

    finish format but a bug

--- format.c
+++ format.c
@@ -4,18 +4,30 @@
 #include <memory.h>
 #include "fs.h"
 #define BLOCKSIZE 1024
-#define BLOCK_SIZE_BITS 10	// 数据块长度所占比特位数。
-#define NR_OPEN 20		
+#define BLOCK_SIZE_BITS 10 // 数据块长度所占比特位数。
+#define NR_OPEN 20
 #define NR_INODE 32
 #define NR_FILE 64
-#define INODE_SIZE 48
-off_t s_format(int fd,long file_sys_size,struct super_block *super_block);
+//#define BLOCKCOUNT 500
+//#define INODE_SIZE 48
+int block_format(int fd);
+off_t set_empty_block(int fd);
+off_t b_bitmap_format(int fd);
+off_t i_bitmap_format(int fd);
+off_t i_format(int fd);
+off_t s_format(int fd, long file_sys_size, struct d_super_block *super_block);
 
-
+off_t i_start;
+off_t i_bitmap_start;
+off_t b_bitmap_start;
+off_t b_start;
 off_t curpos;
+int file_sys_size;
 int main(int argc, char *argv[])
 {
     int fd;
+    struct d_super_block super_block;
+    //struct d_inode *inode;
     if (argc != 3)
     {
         return -1;
@@ -25,55 +37,99 @@ int main(int argc, char *argv[])
         printf("file has been create\n");
         return -1;
     }
-    else                //文件已经创建
+    else //文件已经创建
     {
-        if((fd=open(argv[1],O_RDWR))==-1)
+        if ((fd = open(argv[1], O_RDWR)) == -1)
         {
             printf("open error\n");
             return -1;
         }
-
     }
-    long file_sys_size;
-    struct d_super_block *super_block;
-    struct d_inode *inode;
-    if((file_sys_size=atoi(argv[1]))==-1)
+    // for(int i=0;i<3;i++)
+    //     printf("%s\n",argv[i]);
+
+    if ((file_sys_size = (int)atoi(argv[2])) == -1)
     {
         printf("filesize wrong\n");
         return -1;
     }
-    printf("fileSystem size is %ld\nsuper_block size is %ld\n",
-        file_sys_size,sizeof(struct d_super_block));
-    s_format(fd,file_sys_size,&super_block);
+    printf("fileSystem size is %d\n",
+           file_sys_size);
+    i_start = s_format(fd, file_sys_size, &super_block);
+    printf("super block has been formated\n");
 
-}
-
-off_t s_format(int fd,long file_sys_size,struct super_block *super_block){
-    
-
-    super_block->s_ninodes=0;
-    super_block->s_imap_blocks=1;
-    super_block->s_zmap_blocks=1;
-    super_block->s_firstdatazone=2;
-    super_block->s_nzones=(file_sys_size/BLOCKSIZE)-super_block->s_imap_blocks-super_block->s_zmap_blocks
-                            -INODE_SIZE;
-    super_block->s_max_size=7*BLOCKSIZE+(BLOCKSIZE/sizeof(unsigned short))*BLOCKSIZE+
-                            (BLOCKSIZE/sizeof(unsigned short))*(BLOCKSIZE/sizeof(unsigned short))*BLOCKSIZE;
-    curpos=lseek(fd,0,SEEK_SET);
-    set_empty_block(fd,0);
-    write(fd,super_block,sizeof(super_block));
-    curpos=lseek(fd,0,SEEK_CUR);
+    i_bitmap_start = i_format(fd);
+    printf("inodes has been formated\n");
+    b_bitmap_start = i_bitmap_format(fd);
+    b_start = b_bitmap_format(fd);
+    printf("bitmap has been format\n");
+    block_format(fd);
+    printf("inode start:%ld\ni_bitmap_start:%ld\nb_bitmap_start:%ld\nb_start:%ld\n",
+           i_start, i_bitmap_start, b_bitmap_start, b_start);
+    fflush(stdout);
+    printf("inodes include %ldK;inode bitmap includes %ldK;block bitmap includes %ldK;block includes %ldK\n",
+            (i_bitmap_start-i_start)/1024,(b_bitmap_start-i_bitmap_start)/1024,(b_start-b_bitmap_start)/1024,
+            (curpos-b_start)/1024);
     return 0;
 }
-off_t i_format(int fd,struct d_inode *inode){
-    set_empty_block(fd,BLOCKSIZE);
-
-
+int block_format(int fd)
+{
+    for (int i = 0; i < file_sys_size / BLOCKSIZE; i++)
+    {
+        set_empty_block(fd);
+    }
+    return 0;
 }
-void set_empty_block(int fd,off_t offset){
-    char empty_block[BLOCKSIZE]={0};
-    lseek(fd,offset,SEEK_SET);
-    write(fd,empty_block,sizeof(empty_block));
-    curpos=lseek(fd,0,SEEK_CUR);
-    return;
+off_t i_bitmap_format(int fd)
+{
+    b_bitmap_start = set_empty_block(fd);  
+    return curpos;
+}
+off_t b_bitmap_format(int fd)
+{
+    char buf[BLOCKSIZE] = {0};
+
+    b_start = set_empty_block(fd);
+    for (int i = file_sys_size / BLOCKSIZE; i < BLOCKSIZE; ++i)
+    {
+        buf[i] = 1;
+    }
+    // memset(&buf[file_sys_size/BLOCKSIZE],1,BLOCKSIZE-file_sys_size/BLOCKSIZE);
+    write(fd, buf, b_bitmap_start);
+    curpos = lseek(fd, 0, SEEK_CUR);
+
+    return curpos;
+}
+off_t s_format(int fd, long file_sys_size, struct d_super_block *super_block)
+{
+
+    super_block->s_ninodes = 0;
+    super_block->s_imap_blocks = 1;
+    super_block->s_zmap_blocks = 1;
+    super_block->s_firstdatazone = 2;
+    super_block->s_nzones = (file_sys_size / BLOCKSIZE) - super_block->s_imap_blocks - super_block->s_zmap_blocks - sizeof(struct d_inode);
+    super_block->s_max_size = 7 * BLOCKSIZE + (BLOCKSIZE / sizeof(unsigned short)) * BLOCKSIZE +
+                              (BLOCKSIZE / sizeof(unsigned short)) * (BLOCKSIZE / sizeof(unsigned short)) * BLOCKSIZE;
+    curpos = lseek(fd, 0, SEEK_SET);
+    set_empty_block(fd);
+    lseek(fd, 0, SEEK_SET);
+    write(fd, super_block, sizeof(super_block));
+    curpos = lseek(fd, BLOCKSIZE, SEEK_SET);
+    return curpos;
+}
+off_t i_format(int fd)
+{
+    for (int i = 0; i < file_sys_size / BLOCKSIZE; ++i)
+    {
+        set_empty_block(fd);
+    }
+    return curpos;
+}
+off_t set_empty_block(int fd)
+{
+    char empty_block[BLOCKSIZE] = {0};
+    write(fd, empty_block, sizeof(empty_block));
+    curpos = lseek(fd, 0, SEEK_CUR);
+    //printf("curpos is %ld\n", curpos);
+    return curpos;
 }
--- fs.h
+++ fs.h
@@ -87,53 +87,53 @@ struct d_super_block
 };
 
 
-//// 以下是文件系统操作管理用的函数原型。
-// 将i 节点指定的文件截为0。
-extern void truncate (struct m_inode *inode);
-// 逻辑块(区段，磁盘块)位图操作。取数据块block 在设备上对应的逻辑块号。
-extern int bmap (struct m_inode *inode, int block);
-// 创建数据块block 在设备上对应的逻辑块，并返回在设备上的逻辑块号。
-extern int create_block (struct m_inode *inode, int block);
-// 获取指定路径名的i 节点号。
-extern struct m_inode *namei (const char *pathname);
-// 根据路径名为打开文件操作作准备。
-extern int open_namei (const char *pathname, int flag, int mode,
-		       struct m_inode **res_inode);
-// 释放一个i 节点(回写入设备)。
-extern void iput (struct m_inode *inode);
-// 从设备读取指定节点号的一个i 节点。
-extern struct m_inode *iget (int dev, int nr);
-// 从i 节点表(inode_table)中获取一个空闲i 节点项。
-extern struct m_inode *get_empty_inode (void);
-// 获取（申请一）管道节点。返回为i 节点指针（如果是NULL 则失败）。
-extern struct m_inode *get_pipe_inode (void);
-// 在哈希表中查找指定的数据块。返回找到块的缓冲头指针。
-extern struct buffer_head *get_hash_table (int dev, int block);
-// 从设备读取指定块（首先会在hash 表中查找）。
-extern struct buffer_head *getblk (int dev, int block);
-// 读/写数据块。
-extern void ll_rw_block (int rw, struct buffer_head *bh);
-// 释放指定缓冲块。
-extern void brelse (struct buffer_head *buf);
-// 读取指定的数据块。
-extern struct buffer_head *bread (int dev, int block);
-// 读4 块缓冲区到指定地址的内存中。
-extern void bread_page (unsigned long addr, int dev, int b[4]);
-// 读取头一个指定的数据块，并标记后续将要读的块。
-extern struct buffer_head *breada (int dev, int block, ...);
-// 向设备dev 申请一个磁盘块（区段，逻辑块）。返回逻辑块号
-extern int new_block (int dev);
-// 释放设备数据区中的逻辑块(区段，磁盘块)block。复位指定逻辑块block 的逻辑块位图比特位。
-extern void free_block (int dev, int block);
-// 为设备dev 建立一个新i 节点，返回i 节点号。
-extern struct m_inode *new_inode (int dev);
-// 释放一个i 节点（删除文件时）。
-extern void free_inode (struct m_inode *inode);
-// 刷新指定设备缓冲区。
-extern int sync_dev (int dev);
-// 读取指定设备的超级块。
-extern struct super_block *get_super (int dev);
-extern int ROOT_DEV;
+// //// 以下是文件系统操作管理用的函数原型。
+// // 将i 节点指定的文件截为0。
+// extern void truncate (struct m_inode *inode);
+// // 逻辑块(区段，磁盘块)位图操作。取数据块block 在设备上对应的逻辑块号。
+// extern int bmap (struct m_inode *inode, int block);
+// // 创建数据块block 在设备上对应的逻辑块，并返回在设备上的逻辑块号。
+// extern int create_block (struct m_inode *inode, int block);
+// // 获取指定路径名的i 节点号。
+// extern struct m_inode *namei (const char *pathname);
+// // 根据路径名为打开文件操作作准备。
+// extern int open_namei (const char *pathname, int flag, int mode,
+// 		       struct m_inode **res_inode);
+// // 释放一个i 节点(回写入设备)。
+// extern void iput (struct m_inode *inode);
+// // 从设备读取指定节点号的一个i 节点。
+// extern struct m_inode *iget (int dev, int nr);
+// // 从i 节点表(inode_table)中获取一个空闲i 节点项。
+// extern struct m_inode *get_empty_inode (void);
+// // 获取（申请一）管道节点。返回为i 节点指针（如果是NULL 则失败）。
+// extern struct m_inode *get_pipe_inode (void);
+// // 在哈希表中查找指定的数据块。返回找到块的缓冲头指针。
+// extern struct buffer_head *get_hash_table (int dev, int block);
+// // 从设备读取指定块（首先会在hash 表中查找）。
+// extern struct buffer_head *getblk (int dev, int block);
+// // 读/写数据块。
+// extern void ll_rw_block (int rw, struct buffer_head *bh);
+// // 释放指定缓冲块。
+// extern void brelse (struct buffer_head *buf);
+// // 读取指定的数据块。
+// extern struct buffer_head *bread (int dev, int block);
+// // 读4 块缓冲区到指定地址的内存中。
+// extern void bread_page (unsigned long addr, int dev, int b[4]);
+// // 读取头一个指定的数据块，并标记后续将要读的块。
+// extern struct buffer_head *breada (int dev, int block, ...);
+// // 向设备dev 申请一个磁盘块（区段，逻辑块）。返回逻辑块号
+// extern int new_block (int dev);
+// // 释放设备数据区中的逻辑块(区段，磁盘块)block。复位指定逻辑块block 的逻辑块位图比特位。
+// extern void free_block (int dev, int block);
+// // 为设备dev 建立一个新i 节点，返回i 节点号。
+// extern struct m_inode *new_inode (int dev);
+// // 释放一个i 节点（删除文件时）。
+// extern void free_inode (struct m_inode *inode);
+// // 刷新指定设备缓冲区。
+// extern int sync_dev (int dev);
+// // 读取指定设备的超级块。
+// extern struct super_block *get_super (int dev);
+// extern int ROOT_DEV;
 
-// 安装根文件系统。
-extern void mount_root (void);
+// // 安装根文件系统。
+// extern void mount_root (void);
